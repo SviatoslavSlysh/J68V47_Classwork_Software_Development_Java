e6f9b138082198492e56e5909d5cfafeeb2916e0
Sviatoslav Slysh <30193901@nescol.ac.uk>
2023-09-07 14:47:06 +0100

    haiku

---
+++ src/haiku.java
@@ -0,0 +1,7 @@
+public class haiku {
+    public static void main(String[] args) {
+        System.out.println("Shells rain down on cities");
+        System.out.println("Death and destruction everywhere");
+        System.out.println("Hope for peace still burns");
+    }
+}
