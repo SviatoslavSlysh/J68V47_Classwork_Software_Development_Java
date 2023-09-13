8186df6aa6538cb6915a3794a1f0e43fd3abf8f6
Sviatoslav Slysh <30193901@nescol.ac.uk>
2023-09-13 11:53:28 +0100

    Lesson 3

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 6; i++) {
+//            System.out.println(i + "Yes");
+//        }
+//    }
+//}
+
+
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int i = 0;
+//        while
+//        (i < 6) {
+//            System.out.print(i);
+//            i++
+//            ;
+//        }
+//        }
+//    }
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[] cars = {"Ford", "BMW", "Lexus", "Lotus"};
+        Integer[] price = {10000, 50000, 30000, 40000};
+        for (int i=0; i < cars.length; i++) {
+            System.out.println("Mark: " + cars[i]  + " = Cost: " + price[i]);
+            
+        }
+
+
+    }
+}
+
+
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner ask = new Scanner(System.in);
+//        String quest = ask.nextLine();
+//        System.out.println("Ask quest..");
+//        while
+//        }
+//    }
